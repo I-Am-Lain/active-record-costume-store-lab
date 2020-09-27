0f6471a5d3d0025a5bd1b1747ff5cf41df049539
I-Am-Lain <vetter.william@gmail.com>
2020-09-27 01:15:36 -0500

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,3 +9,15 @@
 # value types according to the spec
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+
+class CreateCostumes < ActiveRecord::Migration[4.2]
+    def change
+        create_table :costumes do |c|
+            c.string :name
+            c.float :price
+            c.string :image_url
+            c.integer :size
+            c.timestamps
+        end
+    end
+end
